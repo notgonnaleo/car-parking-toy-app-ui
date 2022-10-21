791f5894fa6f69d6444e03d42dfa8e78620cce1c
notgonnaleo <lbruni10@gmail.com>
2022-10-21 17:38:25 -0300

    listagemveiculos: criado rota/conexao direta com a endpoint de create no backend

--- routes/veiculos.js
+++ routes/veiculos.js
@@ -35,4 +35,38 @@ router.get('/ListagemVeiculos', async ( req, res, next ) =>
     }
 })
 
+router.post('/NovoVeiculo', async ( req, res, next ) => 
+{
+    try
+    {
+
+        var CreateVeiculo = await veiculosController.CreateVeiculo(req, res);
+
+        // Implementar armazenamento em cache : INICIO
+        var Veiculos = await veiculosController.ListagemVeiculos(req, res);
+        var Modelos = await veiculosController.ListagemModelos(req, res);
+        var Cores = await veiculosController.ListagemCores(req, res);
+        
+        var tableVeiculos = [
+                            "Número da Placa",
+                            "Modelo do Veículo",
+                            "Cor do Veículo",
+                            "Data do Cadastro",
+                            "Quilometragem"
+                            ]
+
+        // Implementar armazenamento em cache : FIM  
+
+        res.render(viewpath+'/Veiculos/veiculos', {
+            ListagemModelos: Modelos,
+            ListagemCores: Cores,
+            ListagemVeiculos: Veiculos,
+            tableVeiculos: tableVeiculos
+        });
+    }
+    catch(error){
+        console.log(error)
+    }
+})
+
 module.exports = router
